8d5bf8a2dd5afbe586777bafeaa0083caec69afe
thuarte <anhthu.afas97@gmail.com>
2021-05-16 12:35:58 +0200

    trial

--- js/copyright.js
+++
@@ -1,7 +0,0 @@
-﻿// Copyright information
-$(document).ready(function(){
-  var copyright = '&copy;';
-  var author = 'Nguyễn Anh Thư';
-  var year = new Date().getFullYear();
-  $("#copyright").html(copyright + ' ' + author + ' ' + year);
-});
