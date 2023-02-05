2a7f5136a88f573dd84ed8c4f1e1cb7ea40644bf
thuarte <78174209+thuarte@users.noreply.github.com>
2023-02-05 18:02:19 +0700

    Gallery

--- js/albums_acrylic.js
+++ js/albums_acrylic.js
@@ -6,15 +6,21 @@ function getAlbumsAcrylic() {
       image:'pics/gallery/acrylic/summer_love.jpg',
       pictures: [
         {
-          title:'Frozen Flower',
+          title:'Summer love',
           url:'pics/gallery/acrylic/summer_love.jpg',
-          width:30, height:40, year:2021, tag:'SOLD',
+          width:65, height:81, year:2022, tag:'SOLD',
         },
         {
-          title:'Frozen Flower II',
+          title:'Tulips',
           url:'pics/gallery/acrylic/tulips.jpg',
           comment:'Acrylic with plant fiber',
-          width:60, height:80, year:2022,
+          width:50, height:70, year:2021,
+        },
+		{
+          title:'Azalea',
+          url:'pics/gallery/acrylic/azalea.jpg',
+          comment:'Acrylic on canvas',
+          width:80, height:80, year:2023,
         },
       ]
     },
