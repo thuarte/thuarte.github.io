0beba652e2f9cb82c7c2a7b09dd2f4cbfbe8637f
thuarte <78174209+thuarte@users.noreply.github.com>
2023-02-05 18:56:37 +0700

    Update albums_acrylic.js

--- js/albums_acrylic.js
+++ js/albums_acrylic.js
@@ -6,9 +6,33 @@ function getAlbumsAcrylic() {
       image:'pics/gallery/acrylic/summer_love.jpg',
       pictures: [
         {
+          title:'Azalea',
+          url:'pics/gallery/acrylic/azalea.jpg',
+          comment:'Acrylic on canvas',
+          width:80, height:80, year:2023,
+        },
+		{
           title:'Summer love',
           url:'pics/gallery/acrylic/summer_love.jpg',
           width:65, height:81, year:2022, tag:'SOLD',
+        },
+		{
+          title:"Flower's song",
+          url:'pics/gallery/acrylic/flowers.jpg',
+          comment:'Acrylic on canvas',
+          width:65, height:81, year:2021,
+        },
+		{
+          title:'La vie en rose!',
+          url:'pics/gallery/acrylic/la_vie_en_rose.jpg',
+          comment:'Acrylic on canvas',
+          width:60, height:60, year:2021,
+        },
+		{
+          title:'Still life',
+          url:'pics/gallery/acrylic/still_life.jpg',
+          comment:'Acrylic on canvas',
+          width:60, height:60, year:2021,
         },
         {
           title:'Tulips',
@@ -17,10 +41,16 @@ function getAlbumsAcrylic() {
           width:50, height:70, year:2021,
         },
 		{
-          title:'Azalea',
-          url:'pics/gallery/acrylic/azalea.jpg',
+          title:'Lotus',
+          url:'pics/gallery/acrylic/lotus.jpg',
           comment:'Acrylic on canvas',
-          width:80, height:80, year:2023,
+          width:65, height:81, year:2021,
+        },
+		{
+          title:'Joy',
+          url:'pics/gallery/acrylic/joy.jpg',
+          comment:'Acrylic on canvas',
+          width:40, height:30, year:2021, tag:'SOLD',
         },
       ]
     },
