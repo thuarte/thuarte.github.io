990672d2c7a1265a7e7ab2a0786204e0ea4e8a76
thuarte <78174209+thuarte@users.noreply.github.com>
2023-02-12 12:18:00 +0700

    Update albums_acrylic.js

--- js/albums_acrylic.js
+++ js/albums_acrylic.js
@@ -57,10 +57,66 @@ function getAlbumsAcrylic() {
     {
       name:'Mountains',
       image:'pics/gallery/acrylic/sacred_mountain3.jpg',
+	        pictures: [
+		{
+          title:'Sacred Mountains',
+          url:'pics/gallery/acrylic/sacred_mountains.jpg',
+          comment:'Acrylic on canvas',
+          width:50, height:70, year:2021,
+        },
+		{
+          title:'Tranquilité',
+          url:'pics/gallery/acrylic/tranquillité.jpg',
+          comment:'Acrylic on canvas',
+          width:60, height:60, year:2021,
+        },
+        {
+          title:'Sacred Mountain I',
+          url:'pics/gallery/acrylic/sacred_mountain1.jpg',
+          comment:'Acrylic on canvas',
+          width:65, height:81, year:2021,
+        },
+		{
+          title:'Sacred Mountain II',
+          url:'pics/gallery/acrylic/sacred_mountain2.jpg',
+          comment:'Acrylic on canvas',
+          width:65, height:81, year:2021,
+        },
+		{
+          title:'Sacred Mountain III',
+          url:'pics/gallery/acrylic/sacred_mountain3.jpg',
+          comment:'Acrylic on canvas',
+          width:65, height:81, year:2021,  tag:'SOLD',
+        },
+		{
+          title:'Sacred Mountain IV',
+          url:'pics/gallery/acrylic/sacred_mountain4.jpg',
+          comment:'Acrylic on canvas',
+          width:60, height:80, year:2021,
+        },
+		{
+          title:'Sacred Mountain V',
+          url:'pics/gallery/acrylic/sacred_mountain5.jpg',
+          comment:'Acrylic on canvas',
+          width:65, height:81, year:2021,
+        },
+		{
+          title:'In the hall of Mountain King',
+          url:'pics/gallery/acrylic/in_the_hall_of_mountain_king.jpg',
+          comment:'Acrylic on canvas',
+          width:81, height:65, year:2021,
+        },
+		{
+          title:'Eternity',
+          url:'pics/gallery/acrylic/eternity.jpg',
+          comment:'Acrylic on canvas',
+          width:100, height:80, year:2021, tag:'SOLD',
+        },
+	  ]
     },
 	  
     {
-      name:'Forests',
+      name:'Enchanted Forests',
       image:'pics/gallery/acrylic/enchanted_forest2.jpg',
     },
   ];
